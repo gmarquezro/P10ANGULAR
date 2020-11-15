e6c2ee076b18499a7ab61d6d41130f44ed1b55e1
ogonzaloUoc <ogonzalo@uoc.edu>
2020-11-15 15:15:04 +0100

    minor conflict resolution

--- src/app/mock-songs.ts
+++ src/app/mock-songs.ts
@@ -1,40 +1,4 @@
-import { Cancion } from './cancion';
+import { Cancion, clases_de_albums } from './cancion';
 
-export const LIST: Cancion[] = [
-
-    {     
-        titulo: 'Hijo de la luna',
-        autor: 'Mecano',
-        year: '1998',
-        disco: 'Ana,Jose,Nacho',
-        estilo: 'pop' 
-    },
-    {     
-        titulo: 'A dios le pido',
-        autor: 'Juanes',
-        year: '2002',
-        disco: 'Un día normal',
-        estilo: 'pop' 
-    },
-    {     
-        titulo: 'Highway to hell',
-        autor: 'AC/DC',
-        year: '1979',
-        disco: 'Highway to hell',
-        estilo: 'Rock' 
-    },
-    {     
-        titulo: 'La flaca',
-        autor: 'Jarabe de palo',
-        year: '1996',
-        disco: 'La flaca',
-        estilo: 'pop' 
-    },
-    {     
-        titulo: 'Caminando por la vida',
-        autor: 'Melendi',
-        year: '2006',
-        disco: 'Que el cielo espere sentao',
-        estilo: 'pop' 
-    },
+export const CANCIONES: Cancion[] = [
 ];
