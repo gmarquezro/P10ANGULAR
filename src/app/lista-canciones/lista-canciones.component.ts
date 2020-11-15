e4c2567940fad6ddf21a382abb0ebd01e26e8b24
ogonzaloUoc <ogonzalo@uoc.edu>
2020-11-15 16:30:31 +0100

    added search feature and corresponding animation

--- src/app/lista-canciones/lista-canciones.component.ts
+++ src/app/lista-canciones/lista-canciones.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 import { CANCIONES } from '../mock-canciones';
 import { Cancion } from '../cancion';
 import {
@@ -7,46 +7,44 @@ import {
   style,
   animate,
   transition,
-  group
+  query,
+  stagger
   // ... 
-} from '@angular/animations'; 
+} from '@angular/animations';
 
 
 @Component({
   selector: 'app-lista-canciones',
   animations: [
-    trigger('flyInOut', [
-      state('in', style({
-        transform: 'translateX(0)',
-        opacity: 1,
-      })),
-      transition(':enter', [style({
-        width: '0%',
-        transform: 'translateX(50px)',
-        opacity: 0,
-      }),
-      group([
-        animate('1.5s .2s ease', style({
-          transform: 'translateX(0)',
-        })),
-        animate('3s ease', style({
-          opacity: 1,
-          width: '100%',
-        }))
-      ])
-    ]),
-      transition(':leave', [
-        group([
-          animate('0.3s ease', style({
-            transform: 'translateX(50px)',
-          })),
-          animate('0.3s 0.2s ease', style({
-            opacity: 0
-          }))
+    trigger('pageAnimations', [
+      transition(':enter', [
+        query('li, form', [
+          style({ opacity: 0, transform: 'translateX(-100%)' }),
+          stagger(-30, [
+            animate('3s cubic-bezier(0.35, 0, 0.25, 1)', style({ opacity: 1, transform: 'none' }))
+          ])
         ])
       ])
     ]),
-    trigger('openClose', [
+    trigger('filterAnimation', [
+      transition(':enter, * => 0, * => -1', []),
+      transition(':increment', [
+        query(':enter', [
+          style({ opacity: 0, width: '0px' }),
+          stagger(50, [
+            animate('300ms ease-out', style({ opacity: 1, width: '*' })),
+          ]),
+        ], { optional: true })
+      ]),
+      transition(':decrement', [
+        query(':leave', [
+          stagger(50, [
+            animate('300ms ease-out', style({ opacity: 0, width: '0px' })),
+          ]),
+        ])
+      ]),
+    ]),
+    trigger('close', [
       state('closed', style({
         width: '65%',
       })),
@@ -60,20 +58,49 @@ import {
 })
 export class ListaCancionesComponent implements OnInit {
 
-  canciones = CANCIONES;
   selectedCancion: Cancion;
   isOpen = true;
+
+  @HostBinding('@pageAnimations')
+  public animatePage = true;
+
+  _canciones = [];
+  totalCanciones = -1;
+
+  get canciones() {
+    return this._canciones;
+  }
 
   constructor() { }
 
   ngOnInit() {
+    this._canciones = CANCIONES;
+  }
+
+  updateCriteria(criteria: string) {
+    criteria = criteria ? criteria.trim() : '';
+
+    this._canciones = CANCIONES.filter(cancion => cancion.titulo.toLowerCase().includes(criteria.toLowerCase()) // Filto por titulo
+      || cancion.autor.toLowerCase().includes(criteria.toLowerCase()) // Filtro por autor
+      || cancion.titulo_album.toLowerCase().includes(criteria.toLowerCase()) // Filtro por titulo_album
+      || cancion.fecha_publicacion.getFullYear().toString().includes(criteria)); // Filtro por año en fecha_publicacion
+
+    const newTotal = this.canciones.length;
+
+    if (this.totalCanciones !== newTotal) {
+      this.totalCanciones = newTotal;
+    } else if (!criteria) {
+      this.totalCanciones = -1;
+    }
   }
 
   onSelect(cancion: Cancion): void {
     this.selectedCancion = cancion;
   }
+
   close(): void {
-    if(this.isOpen)
+    if (this.isOpen)
       this.isOpen = false;
   }
 }
+
