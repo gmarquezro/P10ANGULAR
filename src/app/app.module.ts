919ceb97013d7e905c01a9117afe34a5edcb3e2e
ogonzaloUoc <ogonzalo@uoc.edu>
2020-11-20 18:05:31 +0100

    added support for angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { CancionComponent } from './cancion/cancion.component';
 import { ListaCancionesComponent } from './lista-canciones/lista-canciones.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { ListaCancionesComponent } from './lista-canciones/lista-canciones.compo
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
