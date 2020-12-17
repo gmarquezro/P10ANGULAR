cb7bc58e60709f8573956386a4d65f5b5690dbd1
¨gerri¨ <gerardmarquez96@gmail.com>
2020-12-17 20:02:33 +0100

    firebase install

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDLoYv1QCwf6qClACF7Ej47ZRwPNDV7Ne0",
+    authDomain: "p10angular.firebaseapp.com",
+    databaseURL: "https://p10angular-default-rtdb.firebaseio.com",
+    projectId: "p10angular",
+    storageBucket: "p10angular.appspot.com",
+    messagingSenderId: "79331530095",
+    appId: "1:79331530095:web:c3f571f45075be5942049a",
+    measurementId: "G-F73F5MBJEH"
+  }
 };
 
 /*
